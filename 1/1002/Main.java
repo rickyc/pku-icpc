ecbf719015ca726d1981f84978d44b88b20637ca
Ricky <rickyc.us@gmail.com>
2010-03-29 08:50:16 -0400

    lookup table version

--- 1/1002/Main.java
+++ 1/1002/Main.java
@@ -1,16 +1,18 @@
 import java.io.File;
-import java.io.IOException;
 import java.lang.Character;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
 
+    public static char[] alpha = { '2','2','2','3','3','3','4','4','4','5','5','5','6','6','6','7','0','7','7','8','8','8','9','9','9'};
+
     public static void main(String[] args) throws IOException {
         //Scanner s = new Scanner(System.in);
         Scanner s = new Scanner(new File("input.txt"));
-        TreeMap<String,Integer> hm = new TreeMap<String,Integer>();
+        TreeMap<String,Integer> tm = new TreeMap<String,Integer>();
         boolean duplicateFound = false;
 
         int numberOfLines = s.nextInt();
@@ -20,16 +22,22 @@ public class Main {
 
             StringBuffer key = new StringBuffer();
             for(int j=0;j<str.length();j++) {
-                key.append(convert(str.charAt(j)));
+                char c = str.charAt(j);
+
+                if(Character.isLetter(c))
+                    key.append(alpha[c-'A']);
+                else if(Character.isDigit(c))
+                    key.append(c);
+
                 if (key.length() == 3) key.append('-');
             }
 
             String sKey = key.toString();
-            if (hm.containsKey(sKey)) {
-                hm.put(sKey,hm.get(sKey)+1);
+            if (tm.containsKey(sKey)) {
+                tm.put(sKey,tm.get(sKey)+1);
                 duplicateFound = true;
             } else
-                hm.put(sKey,new Integer(1));
+                tm.put(sKey,1);
         }
 
         if (!duplicateFound) {
@@ -37,28 +45,11 @@ public class Main {
             System.exit(0);
         }
 
-        for(Map.Entry<String,Integer>e : hm.entrySet()) {
+        for(Map.Entry<String,Integer>e : tm.entrySet()) {
             int v = e.getValue();
             if (v != 1)
                 System.out.println(e.getKey() + " " + v);
         }
-    }
-
-    public static String convert(char c) {
-        if (Character.isDigit(c)) return c + "";
-        else {
-            switch(c) {
-                case 'A': case 'B': case 'C': return "2"; 
-                case 'D': case 'E': case 'F': return "3"; 
-                case 'G': case 'H': case 'I': return "4"; 
-                case 'J': case 'K': case 'L': return "5"; 
-                case 'M': case 'N': case 'O': return "6"; 
-                case 'P': case 'R': case 'S': return "7"; 
-                case 'T': case 'U': case 'V': return "8"; 
-                case 'W': case 'X': case 'Y': return "9"; 
-            }
-        }
-        return ""; 
     }
 }
 
