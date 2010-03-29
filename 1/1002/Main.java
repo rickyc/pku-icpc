3d49b81e183fd7e9f0dea8bc89a54fdd287d75c8
Ricky <rickyc.us@gmail.com>
2010-03-29 08:48:54 -0400

    regex version of 1002

--- 1/1002/Main.java
+++ 1/1002/Main.java
@@ -1,8 +1,51 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    public static boolean duplicateFound;
+
+    public static void main(String[] args) throws IOException {
+        //Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(new File("input.txt"));
+        TreeMap<String,Integer> hm = new TreeMap<String,Integer>();
+        duplicateFound = false;
+
         int numberOfLines = s.nextInt();
+
+        String str = "";
+        for(int i=0;i<numberOfLines;i++) {
+            str = s.next();
+
+            str = str.replaceAll("-*","");
+            str = str.replaceAll("(A|B|C){1}","2");
+            str = str.replaceAll("(D|E|F){1}","3");
+            str = str.replaceAll("(G|H|I){1}","4");
+            str = str.replaceAll("(J|K|L){1}","5");
+            str = str.replaceAll("(M|N|O){1}","6");
+            str = str.replaceAll("(P|R|S){1}","7");
+            str = str.replaceAll("(T|U|V){1}","8");
+            str = str.replaceAll("(W|X|Y){1}","9");
+
+            String k = str.substring(0,3) + '-' + str.substring(3,7);
+            if (hm.containsKey(k)) {
+                hm.put(k,hm.get(k)+1);
+                duplicateFound = true;
+            } else
+                hm.put(k,new Integer(1));
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No duplicates.");
+            System.exit(0);
+        }
+
+        for(Map.Entry<String,Integer>e : hm.entrySet()) {
+            int v = e.getValue();
+            if (v != 1)
+                System.out.println(e.getKey() + " " + v);
+        }
     }
 }
