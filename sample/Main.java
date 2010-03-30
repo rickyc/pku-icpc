fc4b1474be56f3fda93a7b133b97f0cfcce4c46b
Ricky <rickyc.us@gmail.com>
2010-03-30 12:42:35 -0400

    setting up 1005 and adding sample files

---
+++ sample/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        while(sn.hasNext()) {
+            
+        }
+    }
+}
